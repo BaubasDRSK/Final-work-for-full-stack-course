e1438a2af08af47be4a2da06b7375f4ba21203b9
BaubasDRSK <127085633+BaubasDRSK@users.noreply.github.com>
2023-08-14 11:35:26 +0300

    small fix

--- resources/views/stories/hearts-list.blade.php
+++ resources/views/stories/hearts-list.blade.php
@@ -1,13 +1,13 @@
 
 @if (empty($story->loveit))
-    <div data-heart-load data-url={{ route('stories-heartsCount', $story) }} class="heart empty">
+    <div  class="heart empty">
         <i data-heart-action data-heart-action-type="addHeart" data-url="{{ route('stories-heartsAdd', $story) }}"
             class="fa fa-heart" aria-hidden="true">
         </i>
         <span href="#">{{ $heartsCount == 0 ? '#' : $heartsCount }}</span>
     </div>
 @else
-    <div data-heart-load data-url={{ route('stories-heartsCount', $story) }} class="heart full">
+    <div class="heart full">
         <i data-heart-action data-heart-action-type="addHeart" data-url="{{ route('stories-heartsAdd', $story) }}"
             class="fa fa-heart" aria-hidden="true">
         </i>
